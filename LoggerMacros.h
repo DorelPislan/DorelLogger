28cc389c1fb7ea08005bdc71641e1c9ff4aa92ae
Dorel Pîslan <pislan@gmail.com>
2025-03-29 11:08:58 +0200

    Used ErrorCodeType for lastError

--- LoggerMacros.h
+++ LoggerMacros.h
@@ -1,9 +1,7 @@
 #include "Logger.h"
 #include "MessageBuilder.h"
 #include "Os.h"
-
-// clang-format off
-// clang-format on
+#include "Types.h"
 
 #ifndef ESTIMATED_MESSAGE_LENGTH
 #define ESTIMATED_MESSAGE_LENGTH 200
@@ -34,12 +32,23 @@
     }                                                                                    \
   }
 
+#define LOG_ERROR_FMT(aFormat, ...)                               \
+  {                                                               \
+    auto & logger = GET_LOGGER();                                 \
+    if (logger.ShouldLog(DorelLogger::ISink::MessageType::Error)) \
+    {                                                             \
+      ErrorCodeType lastError = DorelLogger::Os::GetLastError();  \
+      lastError;                                                  \
+      logger.LogErrorFmt(SRC_POS, aFormat, __VA_ARGS__);          \
+    }                                                             \
+  }
+
 #define LOG_ERROR(msg)                                                                          \
   {                                                                                             \
     auto & logger = GET_LOGGER();                                                               \
     if (logger.ShouldLog(DorelLogger::ISink::MessageType::Error))                               \
     {                                                                                           \
-      int lastError = DorelLogger::Os::GetLastError();                                          \
+      ErrorCodeType lastError = DorelLogger::Os::GetLastError();                                \
       lastError;                                                                                \
       logger.LogError(SRC_POS, (DorelLogger::MessageBuilder(ESTIMATED_MESSAGE_LENGTH) << msg)); \
     }                                                                                           \
