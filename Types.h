75415a2e05e2c7170fcf00c10c819d3879d68c5e
Dorel Pîslan <pislan@gmail.com>
2025-03-29 11:08:13 +0200

    Added a file that defines types used by librray

---
+++ Types.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#include <type_traits>
+
+// these are taken from "The Old New Thing" blog
+template <typename, typename = void>
+constexpr bool is_type_complete_v = false;
+
+template <typename T>
+constexpr bool is_type_complete_v<T, std::void_t<decltype(sizeof(T))>> = true;
+
+struct ErrorCode;
+using ErrorCodeType =
+  typename std::conditional<is_type_complete_v<ErrorCode>, ErrorCode, int>::type;
+
+class CriticalSection;
+using MutexType =
+  typename std::conditional<is_type_complete_v<CriticalSection>, CriticalSection, std::mutex>::type;
+
+#endif  //_TYPES_H_
