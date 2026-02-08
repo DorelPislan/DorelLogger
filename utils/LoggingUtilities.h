40193a0946d0251055cce009bcecb083720d8175
Dorel Pîslan <pislan@gmail.com>
2026-02-08 23:58:26 +0200

    Added a much simpler MACRO for defining array with striongs of values

--- utils/LoggingUtilities.h
+++ utils/LoggingUtilities.h
@@ -8,7 +8,8 @@ namespace LoggingUtilities
 using ValueStringPair     = std::pair<uint32_t, const wchar_t *>;
 using ValueStringPairSpan = std::span<ValueStringPair>;
 
-std::vector<const wchar_t *> GetFlagsNames(uint32_t aValue, const ValueStringPairSpan & aFlagsInfos);
+std::vector<const wchar_t *> GetFlagsNames(uint32_t                    aValue,
+                                           const ValueStringPairSpan & aFlagsInfos);
 
 const wchar_t * GetFlagNameOr(uint32_t                    aFlag,
                               const ValueStringPairSpan & aFlagsInfos,
@@ -16,5 +17,20 @@ const wchar_t * GetFlagNameOr(uint32_t                    aFlag,
 
 }  // namespace LoggingUtilities
 };  // namespace DorelLogger
+
+// Utility MACROS for easily defining arrays of ValueStringPair, used for
+// easily converting values to strings when logging
+
+// we need 2 level of indirection to be sure that the parameter is
+// expanded before stringizing it
+#define DL_WS_IMPL(x) L##x
+#define DL_WS(x)      DL_WS_IMPL(x)
+
+#define DL_VAL_STR_ENTRY(aVal, aValText) { static_cast<uint32_t>(aVal), DL_WS(aValText) }
+#define DL_VAL_STR(aVal)                 DL_VAL_STR_ENTRY(aVal, #aVal)
+
+#define DL_DEFINE_VALUES_STRINGS_ARRAY(aArrayName, ...) \
+  inline constexpr auto aArrayName =                    \
+    std::to_array<DorelLogger::LoggingUtilities::ValueStringPair>({ __VA_ARGS__ })
 
 #endif  //_DOREL_LOGGER_LOG_UTILITIES_H_
