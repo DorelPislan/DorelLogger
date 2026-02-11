e04df37241b44273b476fde772eb0c358f184d05
Dorel Pîslan <pislan@gmail.com>
2026-02-11 23:22:53 +0200

    Added cpp file for testing each header in-dependency

---
+++ tests/TestHeadersDependency.cpp
@@ -0,0 +1,36 @@
+
+
+// #include "../core/LoggerMacros.h"
+// #include "../core/LoggerProvider.h"
+//
+// #include "../format/Format.h"
+// #include "../format/FormatResolver.h"
+// #include "../format/FormatTraits.h"
+
+// #include "../formatters/AtlSpecific.h"
+// #include "../formatters/StdTypes.h"
+// #include "../formatters/WindowsSpecific.h"
+
+// #include "../logger/Logger.h"
+// #include "../logger/MessageBuilder.h"
+// #include "../logger/GLobalVariables.h"
+
+// #include "../platform/ErrorCode.h"
+// #include "../platform/Os.h"
+//
+// #include "../sinks/ISink.h"
+// #include "../sinks/SinkBase.h"
+// #include "../sinks/StdFileSink.h"
+// #include "../sinks/WinApiFileSink.h"
+// #include "../sinks/WindowsDebugStreamSink.h"
+// #include "../sinks/WinRtFileSink.h"
+
+// #include "../utils/GenericUint32Formatter.h"
+// #include "../utils/LoggingUtilities.h"
+// #include "../utils/StatisticData.h"
+// #include "../utils/TypeExistenceDetector.h"
+// #include "../utils/Types.h"
+
+void TestSomething()
+{
+}
