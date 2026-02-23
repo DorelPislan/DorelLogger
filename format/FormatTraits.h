5d6e86de1045f10fe51c2f32bfe9f6cd5da4e7f0
Dorel Pîslan <pislan@gmail.com>
2026-02-23 23:07:09 +0200

    Added comment

--- format/FormatTraits.h
+++ format/FormatTraits.h
@@ -59,7 +59,7 @@ constexpr std::wstring_view kDayNumberDescriptor = L"DayNo";
 constexpr std::wstring_view kDayNameDescriptor   = L"DayName";
 
 constexpr std::wstring_view kHour24Descriptor = L"Hour24";  // 0..24
-constexpr std::wstring_view kHour12Descriptor = L"Hour12";
+constexpr std::wstring_view kHour12Descriptor = L"Hour12";  // AM or PM
 
 constexpr std::wstring_view kMinuteDescriptor = L"Minute";  // 2 digits
 constexpr std::wstring_view kSecondDescriptor = L"Second";  // 2 digits
